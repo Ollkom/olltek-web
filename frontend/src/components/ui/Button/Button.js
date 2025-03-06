df205c6e3ae76fd33af34f90f06a55b568054700
root <root@Roach>
2025-03-06 16:42:39 +0530

    feature/button-framer-motion - Add Framer Motion animations to Button component

--- frontend/src/components/ui/Button/Button.js
+++ frontend/src/components/ui/Button/Button.js
@@ -1,6 +1,7 @@
 "use client";
 import cx from "classnames";
 import { ButtonIcon } from "@/assets/images";
+import { motion } from "framer-motion";
 
 const Button = (props) => {
   const {
@@ -17,15 +18,15 @@ const Button = (props) => {
         "flex relative": icon,
       })}
     >
-      <button
+      <motion.button
         className={cx(
           "rounded-full font-medium text-base text-black",
           {
             "pointer-events-none opacity-50": disabled,
 
-            "text-darkGrayText bg-white px-4 py-2.5 hover:bg-lightBlue border border-white hover:border-lightBlue hover:text-white active:bg-darkBlue transition-colors duration-300 ease-in-out":
+            "text-darkGrayText bg-white px-5 py-2.5 hover:bg-lightBlue border border-darkGrayText hover:border-lightBlue hover:text-white active:bg-darkBlue transition-colors duration-300 ease-in-out":
               variant === "secondary",
-            "bg-lightBlue text-white px-4 py-2.5 border border-lightBlue hover:border-lightBlue hover:bg-white hover:text-lightBlue active:bg-lightGrayBackground transition-colors duration-300 ease-in-out":
+            "bg-lightBlue text-white px-5 py-2.5 border border-lightBlue hover:border-lightBlue hover:bg-white hover:text-lightBlue active:bg-lightGrayBackground transition-colors duration-300 ease-in-out":
               variant === "primary",
             "bg-gradient-to-r from-[#08B1F6] to-[#2F4BDF] py-1.5 px-8 text-white text-2xl 2xl:text-3xl":
               variant === "subsecondary",
@@ -37,6 +38,9 @@ const Button = (props) => {
         )}
         disabled={disabled}
         {...rest}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        transition={{ type: "spring", stiffness: 400, damping: 10 }}
       >
         {variant === "gradient" ? (
           <span className="flex w-full md:w-auto bg-white text-white rounded-full transition-colors duration-300 ease-in-out  hover:text-white hover:bg-gradient-to-r from-[#08B1F6] to-[#2F4BDF]">
@@ -47,7 +51,7 @@ const Button = (props) => {
         ) : (
           children
         )}
-      </button>
+      </motion.button>
 
       {icon && (
         <ButtonIcon className="absolute top-[17px] right-3 md:right-6" />
