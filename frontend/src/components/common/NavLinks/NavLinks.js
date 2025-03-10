df59eb18a3ff609e6d30a19cbf99c9a72b5304c6
root <root@Roach>
2025-03-10 22:07:12 +0530

    feature/header-nav-desktop - Refactor NavLinks component to use useNavbar hook

--- frontend/src/components/common/NavLinks/NavLinks.js
+++ frontend/src/components/common/NavLinks/NavLinks.js
@@ -1,6 +1,5 @@
 "use client";
 import Image from "next/image";
-import { useState, useCallback, useRef, useEffect } from "react";
 import Link from "next/link";
 import {
   IconChevronDown,
@@ -8,46 +7,43 @@ import {
 import { getStrapiMedia } from "@/utils/api-helpers";
 import cx from "classnames";
 import { AdvertisementSlider } from "@/components/common";
+import useNavbar from "@/hooks/common/useNavbar/useNavbar";
+import { useRef } from "react";
 
 const NavLinks = (props) => {
   const { links, advertisements } = props;
-  const [hoverMenuItem, setHoverMenuItem] = useState(null);
+  const { menuState, setMenuState } = useNavbar();
+  const { activeMenu } = menuState;
+  let hoverTimer = useRef(null);
 
-  const hoverEnterTimeout = useRef(null);
-  const hoverLeaveTimeout = useRef(null);
+  const handleMouseEnter = (link) => {
+    clearTimeout(hoverTimer.current);
+    setMenuState(prev => ({
+      ...prev,
+      activeMenu: link,
+    }));
+  };
 
-
-  // set hover menu item and delay to show the hover menu
-  const onHoverEnterHandler = useCallback(
-    (linkId) => {
-      clearTimeout(hoverLeaveTimeout.current);
-
-      hoverEnterTimeout.current = setTimeout(() => {
-        setHoverMenuItem(linkId);
-      }, 50);
-    },
-  );
-
-  // reset hover menu item and delay to hide the hover menu
-  const onHoverLeaveHandler = useCallback(() => {
-    clearTimeout(hoverEnterTimeout.current);
-
-    hoverLeaveTimeout.current = setTimeout(() => {
-      setHoverMenuItem(null);
+  const handleMouseLeave = () => {
+    hoverTimer.current = setTimeout(() => {
+      setMenuState(prev => ({
+        ...prev,
+        activeMenu: null,
+      }));
     }, 500);
-  }, []);
+  };
 
   return (
     <>
       {links?.map((link) => {
         const hasNavigationLinks = link?.navigations?.data?.length > 0;
-        const isMenuOpen = hoverMenuItem === link?.id;
+        const isMenuOpen = activeMenu?.id === link?.id;
 
         return (
           <li
             className="group"
-            onMouseEnter={() => onHoverEnterHandler(link?.id)}
-            onMouseLeave={onHoverLeaveHandler}
+            onMouseEnter={() => handleMouseEnter(link)}
+            onMouseLeave={handleMouseLeave}
             key={link?.id}
           >
             {/* Level 0 */}
@@ -63,8 +59,8 @@ const NavLinks = (props) => {
                 className={cx(
                   "absolute top-[101%] left-0 right-0 z-50 w-full flex justify-end md:max-w-[720px] lg:max-w-[900px] xl:max-w-[1080px] 2xl:max-w-[1680px] mx-auto transition ease-out duration-500 transform",
                   {
-                    "opacity-100 visible": hoverMenuItem === link?.id,
-                    "opacity-0 invisible": hoverMenuItem !== link?.id,
+                    "opacity-100 visible": isMenuOpen,
+                    "opacity-0 invisible": !isMenuOpen,
                   }
                 )}
               >
@@ -80,11 +76,15 @@ const NavLinks = (props) => {
                             key={sublink.id}
                             href={slug || "/"}
                             className="group/sublink flex flex-shrink-0 items-center gap-4 transition-colors md:w-[45%]"
-                            onClick={onHoverLeaveHandler}
+                            onClick={() => {
+                              setMenuState(prev => ({
+                                ...prev,
+                                activeMenu: null
+                              }));
+                            }}
                           >
                             {iconUrl && (
                               <div className="w-10 h-10 rounded-full flex flex-shrink-0 items-center justify-center bg-darkGrayText group-hover/sublink:bg-lightBlue group-active/sublink:bg-lightBlue">
-
                                 <Image
                                   src={getStrapiMedia(iconUrl)}
                                   width={24}
@@ -92,7 +92,6 @@ const NavLinks = (props) => {
                                   alt={heading || `Icon ${sublink.id}`}
                                   className="w-6 h-6"
                                 />
-
                               </div>
                             )}
                             {heading && (
@@ -109,7 +108,9 @@ const NavLinks = (props) => {
                     <AdvertisementSlider
                       advertisements={advertisements}
                       isMenuOpen={isMenuOpen}
-                      hoverMenuItem={hoverMenuItem} />
+                      hoverMenuItem={activeMenu}
+                      closeMenu={handleMouseLeave}
+                    />
                   </div>
                 </div>
               </div>
