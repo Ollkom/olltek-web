7a8c38e78c0bc4e61cddd5957afbb068cbc3e6fe
root <root@Roach>
2025-03-11 20:33:40 +0530

    feature/blog-page-post-routes - Implement blog page and post routes with dynamic rendering and static generation

---
+++ frontend/src/app/blog/[post]/page.js
@@ -0,0 +1,51 @@
+import { notFound } from "next/navigation";
+import { getGlobal, getPageArticles, getPostBySlug } from "@/utils/api-loaders";
+import { Post, PostHeader, SimilarPosts } from "@/components/blog";
+import { InternalContact } from "@/components/forms";
+import { Testimonials } from "@/components/common";
+
+export default async function PostRoute({ params, searchParams }) {
+  const { post } = await params;
+
+  const [data, global] = await Promise.all([
+    getPostBySlug(post, "articles"),
+    getGlobal()
+  ]);
+  const { title, cover, description, category, publishedAt } = data?.data?.[0]?.attributes || [];
+  const backgroundImage = global?.data?.attributes?.backgroundImage;
+  const testimonials = global?.data?.attributes?.testimonials;
+  if (data?.data?.length === 0) return notFound();
+
+  return (
+    <>
+      <PostHeader
+        title={title}
+        category={category}
+        cover={cover?.data?.attributes}
+        description={description}
+        publishedAt={publishedAt}
+      />
+      <div className="container px-4 md:px-0 py-4 md:py-8">
+        <Post data={data?.data?.[0]} />
+        <SimilarPosts postId={data?.data?.[0]?.id} categorySlug={category?.data?.attributes?.slug} searchParams={searchParams} />
+      </div>
+      <InternalContact
+        data={{
+          title: "Contact Us",
+          description: "We're here to help you with any questions or concerns you may have. Please fill out the form below and we'll get back to you as soon as possible.",
+          picture: backgroundImage
+        }}
+      />
+      <Testimonials
+        data={testimonials}
+      />
+    </>
+  );
+}
+
+export async function generateStaticParams() {
+  const data = await getPageArticles("/articles");
+  const { data: posts } = data;
+  const postSlugs = posts?.map((post) => ({ post: post?.attributes?.slug }));
+  return postSlugs;
+}
---
+++ frontend/src/app/blog/page.js
@@ -0,0 +1,43 @@
+import { getGlobal, getPageBySlug } from "@/utils/api-loaders";
+import { AllBlogPosts } from "@/components/blog";
+import { subSectionRenderer } from "@/utils/sub-section-renderer";
+import { InternalContact } from "@/components/forms";
+import { Testimonials } from "@/components/common";
+
+export default async function PageRoute({ searchParams }) {
+  const pageSlug = "blog";
+  const pageArticles = "/articles";
+  const { category, limit } = await searchParams;
+
+  const [page, global] = await Promise.all([
+    getPageBySlug(pageSlug),
+    getGlobal()
+  ]);
+  const contentSections = page?.data?.[0]?.attributes?.contentSections;
+  const backgroundImage = global?.data?.attributes?.backgroundImage;
+  const testimonials = global?.data?.attributes?.testimonials;
+  return (
+    <>
+      {contentSections?.map((section, index) =>
+        subSectionRenderer(section, index, "blog")
+      )}
+      <AllBlogPosts
+        pageSlug={pageSlug}
+        pageArticles={pageArticles}
+        category={category}
+        limit={limit}
+        title="All Blog Posts"
+      />
+      <InternalContact
+        data={{
+          title: "Contact Us",
+          description: "We're here to help you with any questions or concerns you may have. Please fill out the form below and we'll get back to you as soon as possible.",
+          picture: backgroundImage
+        }}
+      />
+      <Testimonials
+        data={testimonials}
+      />
+    </>
+  );
+}
