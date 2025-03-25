af44142a4651b1d97d11cf795bf5181a1f33cc96
root <root@Roach>
2025-03-25 23:33:47 +0530

    feature/backend-career-location-role-team-collection - add collection career, career-role, career-location, and career-team

---
+++ backend/src/api/career-role/services/career-role.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * career-role service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::career-role.career-role');
