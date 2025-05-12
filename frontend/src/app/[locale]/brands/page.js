00fb067c770cc531961f591b86c4b616565a2dc3
root <root@Roach>
2025-05-12 12:25:42 +0530

    feat/contact-form-brands-location-partners - add global contact form to brands, locations, and partners pages

--- frontend/src/app/[locale]/brands/page.js
+++ frontend/src/app/[locale]/brands/page.js
@@ -1,18 +1,22 @@
 import { notFound } from "next/navigation";
-import { getIndustries, getLocations, getPageBySlug } from "@/utils/api-loaders";
+import { getGlobal, getIndustries, getLocations, getPageBySlug } from "@/utils/api-loaders";
 import { OurBrands } from "@/components/brands";
+import { InternalContact } from "@/components/forms";
 
 export default async function PageRoute() {
-  const [page, locations, industries] = await Promise.all([getPageBySlug("brands"), getLocations("/locations"), getIndustries("/industries")])
+  const [page, locations, industries, global] = await Promise.all([getPageBySlug("brands"), getLocations("/locations"), getIndustries("/industries"), getGlobal()])
 
   const contentSections = page?.data?.[0]?.attributes?.contentSections;
   const pageHeader = contentSections?.find(item => item.__component === "layout.page-header") || null;
-
+  const leadForm = global?.data?.attributes?.leadForm;
   if (contentSections?.length === 0) return notFound();
 
   return (
     <>
       {industries && <OurBrands pageHeader={pageHeader} locations={locations} industries={industries} />}
+      {leadForm && <InternalContact
+        leadForm={leadForm}
+      />}
     </>
   );
 }
--- frontend/src/app/[locale]/locations/page.js
+++ frontend/src/app/[locale]/locations/page.js
@@ -1,18 +1,22 @@
 import { notFound } from "next/navigation";
-import { getLocations, getPageBySlug } from "@/utils/api-loaders";
+import { getGlobal, getLocations, getPageBySlug } from "@/utils/api-loaders";
 import { OurLocations } from "@/components/locations";
+import { InternalContact } from "@/components/forms";
 
 export default async function PageRoute() {
-  const [page, locations] = await Promise.all([getPageBySlug("locations"), getLocations("/locations")])
+  const [page, locations, global] = await Promise.all([getPageBySlug("locations"), getLocations("/locations"), getGlobal()])
 
   const contentSections = page?.data[0]?.attributes?.contentSections;
   const pageHeader = contentSections?.find(item => item.__component === "layout.page-header") || null;
-
+  const leadForm = global?.data?.attributes?.leadForm;
 
   if (locations?.data?.length === 0) return notFound();
   return (
     <>
       {locations && <OurLocations data={locations} pageHeader={pageHeader} />}
+      {leadForm && <InternalContact
+        leadForm={leadForm}
+      />}
     </>
   );
 }
